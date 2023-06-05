5a7b1826e3cc843afca3fadb7b024af39e26f276
Andrei Apostol <andrei.apostol@omnimodular.com>
2023-06-05 16:26:09 +0300

    fix relative imports

--- grants_tagger_light/evaluation/__init__.py
+++ grants_tagger_light/evaluation/__init__.py
@@ -1,9 +1,9 @@
 import typer
 
-from evaluate_human import evaluate_human_cli
-from evaluate_mesh_on_grants import evaluate_mesh_on_grants_cli
-from evaluate_model import evaluate_model_cli
-from evaluate_mti import evaluate_mti_cli
+from .evaluate_human import evaluate_human_cli
+from .evaluate_mesh_on_grants import evaluate_mesh_on_grants_cli
+from .evaluate_model import evaluate_model_cli
+from .evaluate_mti import evaluate_mti_cli
 
 evaluate_app = typer.Typer()
 evaluate_app.command("grants")(evaluate_mesh_on_grants_cli)
