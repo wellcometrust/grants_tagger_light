d91a36b2d70462e52e031642c94b7330508a7b30
Andrei Apostol <andrei.apostol@omnimodular.com>
2023-06-21 15:23:05 +0300

    add extra metrics

--- scripts/calc_comparison_metrics.py
+++ scripts/calc_comparison_metrics.py
@@ -3,10 +3,13 @@ import argparse
 import ast
 from grants_tagger_light.models.bert_mesh import BertMesh
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import cohen_kappa_score, jaccard_score
+from pprint import pprint
 
 
 def calc_comparison_metrics(csv_path: str):
+    metric_dict = {}
+
     comparison = pd.read_csv(csv_path)
 
     # Load model to get its id2label
@@ -39,7 +42,22 @@ def calc_comparison_metrics(csv_path: str):
         cohen_kappa = cohen_kappa_score(bertmesh_label_preds, xlinear_label_preds)
         cohen_kappas.append(cohen_kappa)
 
-    print("Average Cohen Kappa: ", sum(cohen_kappas) / len(cohen_kappas))
+    avg_cohen_kappa = sum(cohen_kappas) / len(cohen_kappas)
+
+    metric_dict["avg_cohen_kappa"] = avg_cohen_kappa
+
+    # Count how many tags each model predicts on average for each abstract
+    bertmesh_num_tags = bertmesh_terms.sum() / bertmesh_terms.shape[0]
+    xlinear_num_tags = xlinear_terms.sum() / xlinear_terms.shape[0]
+
+    metric_dict["bertmesh_avg_num_tags"] = bertmesh_num_tags
+    metric_dict["xlinear_avg_num_tags"] = xlinear_num_tags
+
+    metric_dict["jaccard_score"] = jaccard_score(
+        bertmesh_terms, xlinear_terms, average="samples"
+    )
+
+    pprint(metric_dict)
 
 
 if __name__ == "__main__":
