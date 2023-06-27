c1e6034a792d1d3ba48773fe6d22b87154288c4e
Andrei Apostol <andrei.apostol@omnimodular.com>
2023-06-27 16:01:56 +0300

    add arg system for trainer

--- grants_tagger_light/training/train.py
+++ grants_tagger_light/training/train.py
@@ -3,40 +3,23 @@ from transformers import (
     Trainer,
     TrainingArguments,
     EvalPrediction,
+    HfArgumentParser,
 )
 from grants_tagger_light.models.bert_mesh import BertMesh
+from grants_tagger_light.training.train_args import BertMeshTrainingArguments
 from grants_tagger_light.training.dataloaders import load_grants_sample
+from sklearn.metrics import classification_report
 import typer
 import numpy as np
-from sklearn.metrics import classification_report
 
 
-def train_bertmesh(model_key: str, data_path: str, **user_args):
+def train_bertmesh(model_key: str, data_path: str, training_args: TrainingArguments):
     model = BertMesh.from_pretrained(model_key, trust_remote_code=True)
     tokenizer = AutoTokenizer.from_pretrained(model_key)
 
     label2id = {v: k for k, v in model.id2label.items()}
 
     train_dset, val_dset = load_grants_sample(data_path, tokenizer, label2id=label2id)
-
-    training_args = {
-        "output_dir": "model_output",
-        "overwrite_output_dir": True,
-        "num_train_epochs": 1,
-        "per_device_train_batch_size": 4,
-        "per_device_eval_batch_size": 4,
-        "warmup_steps": 500,
-        "weight_decay": 0.01,
-        "learning_rate": 1e-5,
-        "evaluation_strategy": "steps",
-        "eval_steps": 100,
-        "do_eval": True,
-        "label_names": ["labels"],
-    }
-
-    training_args.update(user_args)
-
-    training_args = TrainingArguments(**training_args)
 
     def sklearn_metrics(prediction: EvalPrediction):
         y_pred = prediction.predictions
@@ -80,6 +63,7 @@ train_app = typer.Typer()
 
 @train_app.command()
 def train_bertmesh_cli(
+    ctx: typer.Context,
     model_key: str = typer.Argument(
         ..., help="Pretrained model key. Local path or HF location"
     ),
@@ -87,9 +71,11 @@ def train_bertmesh_cli(
         ...,
         help="Path to data in jsonl format. Must contain text and tags field",
     ),
-    model_save_path: str = typer.Argument(..., help="Path to save model to"),
 ):
-    train_bertmesh(model_key, data_path)
+    parser = HfArgumentParser((BertMeshTrainingArguments,))
+    (training_args,) = parser.parse_args_into_dataclasses(ctx.args)
+
+    train_bertmesh(model_key, data_path, training_args)
 
 
 if __name__ == "__main__":
---
+++ grants_tagger_light/training/train_args.py
@@ -0,0 +1,60 @@
+from transformers import TrainingArguments
+from dataclasses import dataclass, field
+
+
+@dataclass
+class BertMeshTrainingArguments(TrainingArguments):
+    """
+    This class inherits from transformers.TrainingArguments
+    and implements some better defaults for convenience.
+    """
+
+    output_dir: str = field(default="bertmesh-outs/default")
+    overwrite_output_dir: bool = field(default=True)
+
+    evaluation_strategy: str = field(default="epoch")  # no | epoch | steps
+    # eval_steps: int = 1
+
+    save_strategy: str = field(default="epoch")  # no | epoch | steps
+    # save_steps: int = 1
+    save_total_limit: int = field(default=5)
+
+    metric_for_best_model: str = field(
+        default="eval_loss"
+    )  # can switch later to micro-f1
+    greater_is_better: bool = field(default=False)
+    load_best_model_at_end: bool = field(default=True)
+
+    per_device_train_batch_size: int = field(
+        default=8
+    )  # set to 256 in grants-tagger repo
+    per_device_eval_batch_size: int = field(default=16)
+    gradient_accumulation_steps: int = field(default=1)
+    group_by_length: bool = field(default=False)  # TODO test this
+
+    # Learning rate & num train epochs are taken from grants_tagger repo
+    # (BertMeSH paper does not specify these hparams)
+    num_train_epochs: int = field(default=5)
+    learning_rate: float = field(default=1e-4)
+
+    seed: int = field(default=42)
+    data_seed: int = field(default=42)
+
+    optim: str = field(
+        default="adamw_torch_fused"
+    )  # TODO add support for adamw_apex_fused
+
+    fp16: bool = field(default=False)  # TODO test if micro-f1 is maintained
+
+    dataloader_num_workers: int = field(default=8)
+    dataloader_pin_memory: bool = field(default=True)
+
+    gradient_checkpointing: bool = field(default=False)
+
+    auto_find_batch_size: bool = field(default=False)  # TODO test this
+
+    torch_compile: bool = field(default=False)  # TODO make compilation
+    torch_compile_backend: str = field(default="inductor")
+    torch_compile_mode: str = field(
+        default="default"
+    )  # default | reduce-overhead | max-autotune
