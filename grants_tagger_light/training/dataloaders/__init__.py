4c6152c87925e2f2839791c2448071e592c3a82e
Andrei Apostol <andrei.apostol@omnimodular.com>
2023-07-18 19:52:55 +0300

    Revert "revert separate preprocess command"
    This reverts commit 1660ef092399c20e188d6716285cba853d16eccf.

--- grants_tagger_light/training/dataloaders/__init__.py
+++ grants_tagger_light/training/dataloaders/__init__.py
@@ -1,4 +1,3 @@
-from .mesh_json_loader import load_mesh_json
 from .multilabel_collator import MultilabelDataCollator
 
-__all__ = ["load_mesh_json", "MultilabelDataCollator"]
+__all__ = ["MultilabelDataCollator"]
