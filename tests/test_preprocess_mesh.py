c6c3b5bcb411ff897fa21e5b2f52dd5b2ef2c388
Jose J. Martinez <jose.martinez@wayops.eu>
2023-07-24 15:55:51 +0100

    Adapts preprocessing tests to the style of other tests

--- tests/test_preprocess_mesh.py
+++ tests/test_preprocess_mesh.py
@@ -1,15 +1,36 @@
 import json
 import tempfile
-from pathlib import Path
 
 from grants_tagger_light.preprocessing.preprocess_mesh import (
     preprocess_mesh,
 )
 from scripts.jsonl_preprocessing import process_data, mesh_json_to_jsonl
-from loguru import logger
+import pytest
 
 
-FIXTURES_DIR = Path(__file__).resolve().parent / 'fixtures'
+jsonl_data = """{"journal":"dummyJournal","meshMajor":["COVID-19","SARS-CoV-2"],"year":"2023","abstractText":"This is an article about coronavirus.","title":"article1","pmid":"pmid1"}
+{"journal":"dummyJournal","meshMajor":["Malaria"],"year":"2023","abstractText":"This is an article about malaria", "title": "article3", "pmid": "pmid3"}"""
+
+json_data = """{"articles":[
+{"journal":"dummyJournal","meshMajor":["COVID-19","SARS-CoV-2"],"year":"2023","abstractText":"This is an article about coronavirus.","title":"article1","pmid":"pmid1"},
+{"journal":"dummyJournal","meshMajor":["Malaria"],"year":"2023","abstractText":"This is an article about malaria", "title": "article3", "pmid": "pmid3"},
+"""
+
+@pytest.fixture
+def json_data_path():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        data_path = tmpdirname + "/data.json"
+        with open(data_path, "w") as f:
+            f.write(json_data)
+        yield data_path
+
+@pytest.fixture
+def jsonl_data_path():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        data_path = tmpdirname + "/data.jsonl"
+        with open(data_path, "w") as f:
+            f.write(jsonl_data)
+        yield data_path
 
 
 def test_process_data_with_filter_tags():
@@ -56,9 +77,9 @@ def test_process_data_with_filter_years_and_tags():
     assert process_data(item, filter_years=["2020"], filter_tags=["T3"]) is False
 
 
-def test_json_to_jsonl():
+def test_json_to_jsonl(json_data_path):
     output_tmp = tempfile.NamedTemporaryFile(mode="w")
-    mesh_json_to_jsonl(f'{FIXTURES_DIR}/mesh_fixture_head_2.json', output_tmp.name, show_progress=False)
+    mesh_json_to_jsonl(json_data_path, output_tmp.name, show_progress=False)
 
     with open(output_tmp.name, 'r') as f:
         result = [json.loads(jline) for jline in f.read().splitlines()]
@@ -67,8 +88,8 @@ def test_json_to_jsonl():
     output_tmp.close()
 
 
-def test_preprocess_mesh():
-    dset, label2id = preprocess_mesh(data_path=f"{FIXTURES_DIR}/mesh_fixture_head_2.jsonl",
+def test_preprocess_mesh(jsonl_data_path):
+    dset, label2id = preprocess_mesh(data_path=jsonl_data_path,
                                      model_key='',
                                      num_proc=2,
                                      batch_size=1,
@@ -77,6 +98,4 @@ def test_preprocess_mesh():
     assert "test" in dset
     assert len(dset["train"]) == 1
     assert len(dset["test"]) == 1
-    logger.info("label2id")
-    logger.info(label2id)
-    assert len(list(label2id.keys())) == 18
+    assert len(list(label2id.keys())) == 3
