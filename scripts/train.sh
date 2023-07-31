b18dd732cc1fa721a03ce3848c6d961d949414fc
Juan Martinez <jjmcarrascosa@gmail.com>
2023-07-31 16:44:24 +0000

    Updates train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,19 +1,23 @@
 # Run on p2.8xlarge instance
 grants-tagger train bertmesh \
     "" \
-    data/raw/allMeSH_2021.jsonl \
-    --test-size 0.005 \
-    --shards 250 \
+    kk \
+    --test-size 0.0025 \
+    --shards 48 \
     --output_dir bertmesh_outs/pipeline_test/ \
     --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 1 \
     --num_train_epochs 1 \
     --fp16 \
     --torch_compile \
-    --evaluation_strategy no \
-    --save_strategy no \
+    --evaluation_strategy steps \
+    --save_strategy steps \
+    --eval_steps 50000 \
+    --save_steps 50000 \
     --wandb_project wellcome-mesh \
     --wandb_name test-train-all \
-    --wandb_api_key ${WANDB_API_KEY}
+    --wandb_api_key ${WANDB_API_KEY} \
+    --eval_accumulation_steps 20
     #--save_strategy steps 
     #--save_steps 50000
     #--per_device_eval_batch_size 8 \
