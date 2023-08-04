d74c2f6b0b84cd1c98ce90e50590edaeb6dd11cc
Jose J. Martinez <jose.martinez@wayops.eu>
2023-08-04 16:05:20 +0100

    Adds evaluation by epoch

---
+++ scripts/train_by_epochs.sh
@@ -0,0 +1,26 @@
+# Run on g5.12xlargeinstance
+
+# Without preprocessing (on-the-fly)
+SOURCE="data/raw/allMeSH_2021.jsonl"
+
+# After preprocessing first
+# SOURCE="output_folder_from_preprocessing"
+
+grants-tagger train bertmesh \
+    "" \
+    $SOURCE \
+    --output_dir bertmesh_outs/pipeline_test/ \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 1 \
+    --num_train_epochs 5 \
+    --learning_rate 5e-5 \
+    - dropout 1.0 \
+    --warmup_steps 1000 \
+    --fp16 \
+    --torch_compile \
+    --evaluation_strategy epochs \
+    --eval_accumulation_steps 20 \
+    --save_strategy epochs \
+    --wandb_project wellcome-mesh \
+    --wandb_name test-train-all \
+    --wandb_api_key ${WANDB_API_KEY}
