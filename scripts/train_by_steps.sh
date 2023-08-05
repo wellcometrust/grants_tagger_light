5e04318eeb6822115a7b45b4773cf688401c6e86
Jose J. Martinez <jose.martinez@wayops.eu>
2023-08-05 13:06:49 +0100

    Fixes years

--- scripts/train_by_steps.sh
+++ scripts/train_by_steps.sh
@@ -12,8 +12,8 @@ grants-tagger train bertmesh \
     $SOURCE \
     --test-size 10000 \
     --output_dir bertmesh_outs/pipeline_test/ \
-    --train-years 2021 \
-    --test-years 2020 \
+    --train-years 2016,2017,2018,2019 \
+    --test-years 2020,2021 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 1 \
     --num_train_epochs 1 \
