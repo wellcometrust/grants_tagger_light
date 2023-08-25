7da5ac43fc5a03d591c36dbba438871b7e1d57d4
Juan Martinez <jjmcarrascosa@gmail.com>
2023-08-25 19:30:14 +0000

    Adds best params

--- examples/train_by_steps.sh
+++ examples/train_by_steps.sh
@@ -1,38 +1,37 @@
 # Run on g5.12xlargeinstance
 
-# Without preprocessing (on-the-fly)
-SOURCE="data/raw/allMeSH_2021.jsonl"
-
 # If you have already preprocessed the data, you will have a folder. Use the folder instead.
-# SOURCE="output_folder_from_preprocessing"
+SOURCE="output_folder_from_preprocessing"
 # In that case, `test-size`, `train-years` and `test-years` will be taken from the preprocessed folder
 
 grants-tagger train bertmesh \
     "" \
     $SOURCE \
-    --test-size 10000 \
     --output_dir bertmesh_outs/pipeline_test/ \
-    --train-years 2016,2017,2018,2019 \
-    --test-years 2020,2021 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 1 \
     --multilabel_attention True \
-    --freeze_backbone False \
+    --freeze_backbone unfreeze_bias \
     --num_train_epochs 5 \
     --learning_rate 5e-5 \
     --dropout 0.1 \
     --hidden_size 1024 \
-    --warmup_steps 1000 \
-    --max_grad_norm 5.0 \
-    --scheduler-type cosine \
+    --warmup_steps 5000 \
+    --max_grad_norm 1.0 \
+    --scheduler_type cosine \
+    --weight_decay 0.2 \
+    --correct_bias False \
     --threshold 0.25 \
+    --prune_labels_in_evaluation True \
+    --hidden_dropout_prob 0.2 \
+    --attention_probs_dropout_prob 0.2 \
     --fp16 \
     --torch_compile \
     --evaluation_strategy steps \
-    --eval_steps 50000 \
+    --eval_steps 10000 \
     --eval_accumulation_steps 20 \
     --save_strategy steps \
-    --save_steps 50000 \
+    --save_steps 10000 \
     --wandb_project wellcome-mesh \
     --wandb_name test-train-all \
     --wandb_api_key ${WANDB_API_KEY}
