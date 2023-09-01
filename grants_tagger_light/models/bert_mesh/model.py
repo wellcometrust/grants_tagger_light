06c5479cd8635fe2adae357f19ee825d1deb0852
Jose J. Martinez <jose.martinez@wayops.eu>
2023-09-01 14:39:07 +0100

    Fixes black

--- grants_tagger_light/models/bert_mesh/model.py
+++ grants_tagger_light/models/bert_mesh/model.py
@@ -2,6 +2,7 @@ from transformers import AutoModel, PreTrainedModel, BertConfig
 from transformers.modeling_outputs import SequenceClassifierOutput
 import torch
 import torch.nn.functional as F
+
 # from loguru import logger
 
 
