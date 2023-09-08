348ad3bc9c710c7e71ba0ce6066a740e4b71b738
Jose J. Martinez <jose.martinez@wayops.eu>
2023-09-08 12:03:16 +0100

    Prototype for retagging using spacy

--- grants_tagger_light/retagging/retagging.py
+++ grants_tagger_light/retagging/retagging.py
@@ -1,18 +1,15 @@
-import json
 import logging
-import multiprocessing
 import os
 import random
 
 import typer
 from loguru import logger
-import numpy as np
 
 from datasets import load_dataset
 
-from datasets import load_from_disk
 import spacy
-from spacy.util import minibatch, compounding
+from spacy.tokens import DocBin
+from spacy.cli.train import train as spacy_train
 
 retag_app = typer.Typer()
 
@@ -21,37 +18,11 @@ def _load_data(dset: list[str], limit=100, split=0.8):
     """Load data from the IMDB dataset."""
     # Partition off part of the train data for evaluation
     random.Random(42).shuffle(dset)
-    dset = dset[:limit]
-    train_size = int(split * len(dset))
-    test_size = limit - train_size
+    train_size = int(split * limit)
     train_dset = dset[:train_size]
     test_dset = dset[train_size:limit]
     return train_dset, test_dset
 
-
-def evaluate(tokenizer, textcat, texts, cats):
-    docs = (tokenizer(text) for text in texts)
-    tp = 1e-8  # True positives
-    fp = 1e-8  # False positives
-    fn = 1e-8  # False negatives
-    tn = 1e-8  # True negatives
-    for i, doc in enumerate(textcat.pipe(docs)):
-        gold = cats[i]
-        for label, score in doc.cats.items():
-            if label not in gold:
-                continue
-            if score >= 0.5 and gold[label] >= 0.5:
-                tp += 1.
-            elif score >= 0.5 > gold[label]:
-                fp += 1.
-            elif score < 0.5 and gold[label] < 0.5:
-                tn += 1
-            elif score < 0.5 <= gold[label]:
-                fn += 1
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)
-    f_score = 2 * (precision * recall) / (precision + recall)
-    return {'textcat_p': precision, 'textcat_r': recall, 'textcat_f': f_score}
 
 def retag(
     data_path: str,
@@ -78,13 +49,14 @@ def retag(
 
     for tag in tags:
         logging.info(f"Retagging: {tag}")
-        nlp = spacy.blank('en')
+        # nlp = spacy.blank('en')
+        nlp = spacy.load("en_core_web_sm")
 
-        textcat = nlp.create_pipe("textcat")
-        nlp.add_pipe("textcat", last=True)
+        # textcat = nlp.create_pipe("textcat")
+        # nlp.add_pipe("textcat", last=True)
 
-        textcat.add_label(tag)
-        textcat.add_label("O")
+        # textcat.add_label(tag)
+        # textcat.add_label("O")
 
         logging.info(f"Obtaining positive examples for {tag}...")
         positive_dset = dset.filter(
@@ -92,46 +64,47 @@ def retag(
         )
         pos_x_train, pos_x_test = _load_data(positive_dset['abstractText'], limit=100, split=0.8)
 
-        train_data = list(zip(pos_x_train, [{'cats': {tag: True, 'O': False}}]))
-
         logging.info(f"Obtaining negative examples for {tag}...")
         negative_dset = dset.filter(
             lambda x: tag not in x["meshMajor"], num_proc=num_proc
         )
         neg_x_train, neg_x_test = _load_data(negative_dset['abstractText'], limit=100, split=0.8)
 
-        train_data.extend(list(zip(neg_x_train, [{'cats': {tag: False, 'O': True}}])))
-        logging.info(f"Train data size: {len(train_data)}. First example: {train_data[0]}")
+        train_data = DocBin()
+        for doc in nlp.pipe(pos_x_train):
+            doc.cats[tag] = 1
+            doc.cats['O'] = 0
+            train_data.add(doc)
+        for doc in nlp.pipe(neg_x_train):
+            doc.cats[tag] = 0
+            doc.cats['O'] = 1
+            train_data.add(doc)
+        train_data.to_disk("train.spacy")
 
-        test = pos_x_test
-        test_cats = [{'cats': {tag: True, 'O': False}} for _ in range(len(pos_x_test))]
-        test.extend(neg_x_test)
-        test_cats.extend([{'cats': {tag: False, 'O': True}} for _ in range(len(neg_x_test))])
-        logging.info(f"Test data size: {len(test)}")
+        test_data = DocBin()
+        for doc in nlp.pipe(pos_x_test):
+            doc.cats[tag] = 1
+            doc.cats['O'] = 0
+            test_data.add(doc)
+        for doc in nlp.pipe(pos_x_train):
+            doc.cats[tag] = 0
+            doc.cats['O'] = 1
+            test_data.add(doc)
+        test_data.to_disk("test.spacy")
 
-        # get names of other pipes to disable them during training
-        other_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'textcat']
+        logging.info(f"Train data size: {len(train_data)}")
+        logging.info(f"Test data size: {len(test_data)}")
 
-        n_iter = 1
-        with nlp.disable_pipes(*other_pipes):  # only train textcat
-            optimizer = nlp.begin_training()
-            logging.info("Training the model...")
-            logging.info('{:^5}\t{:^5}\t{:^5}\t{:^5}'.format('LOSS', 'P', 'R', 'F'))
-            for i in range(n_iter):
-                losses = {}
-
-            # batch up the examples using spaCy's minibatch
-            batches = minibatch(train_data, size=compounding(4., 32., 1.001))
-            for batch in batches:
-                texts, annotations = zip(*batch)
-                nlp.update(texts, annotations, sgd=optimizer, drop=0.2,
-                           losses=losses)
-            with textcat.model.use_params(optimizer.averages):
-                # evaluate on the dev data split off in load_data()
-                scores = evaluate(nlp.tokenizer, textcat, test, test_cats)
-            logging.info('{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}'  # print a simple table
-                  .format(losses['textcat'], scores['textcat_p'],
-                          scores['textcat_r'], scores['textcat_f']))
+        config_path = "config.cfg"
+        output_model_path = "spacy_textcat"
+        spacy_train(
+            config_path,
+            output_path=output_model_path,
+            overrides={
+                "paths.train": "train.spacy",
+                "paths.dev": "valid.spacy",
+            },
+        )
         break
 
 
