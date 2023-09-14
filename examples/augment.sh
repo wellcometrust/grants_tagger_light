6e01ee00da914c3874ee471535bdd3c11a4fbada
Jose J. Martinez <jjmcarrascosa@gmail.com>
2023-09-14 13:26:11 +0100

    Better error management. Black

--- examples/augment.sh
+++ examples/augment.sh
@@ -1,3 +1,5 @@
-grants-tagger augment mesh [FOLDER_AFTER_PREPROCESSING] [SET_YOUR_OUTPUT_FOLDER_HERE] \
-  --min-examples 25 \
+# Augments data using a file with 1 label per line and years
+grants-tagger augment mesh [FOLDER_AFTER_PREPROCESSING] [SET_YOUR_OUTPUT_FILE] \
+  --tags "Environmental Science" \
+  --examples 25 \
   --concurrent-calls 25
